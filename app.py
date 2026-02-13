588cefb9fa2590d2f5f54f572b3b2538d318afee
veerankilikhith <veerankilikhith1@gmail.com>
2026-02-14 00:14:50 +0530

    Update app.py

--- app.py
+++ app.py
@@ -32,9 +32,12 @@ Will you walk with me… not just now… but for life❤️?
     """
     return render_template("index.html", message=message)
 
+import os
+
 @app.route("/yes")
 def yes():
     return render_template("yes.html")
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=10000)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
