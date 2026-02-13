3db32e849dff48f41c2eacb8d144ad409ffe2196
veerankilikhith <veerankilikhith1@gmail.com>
2026-02-13 20:47:29 +0530

    Update app.py

--- app.py
+++ app.py
@@ -10,14 +10,14 @@ def home():
 Ninnu first time chusina roju nundi destiny ane nammakam start ayyindhi.
 Mana journey chala vintha kadha…
 
-5th class nundi mana iddaram same school lo chadavadam… inter varaku same college lo undadam… kani okkasari kuda proper ga kalavadam jaragaledu. Same centre lo 2 years exams rayadam jarigindhi… still we didn’t notice each other.
+5th class nundi mana iddaram same school lo chadavadam… inter varaku same college lo undadam… kani okkasari kuda chudatam jaragaledu. Same centre lo 2 years exams rayadam jarigindhi… still we didn’t notice each other.
 
 But ippudu alochisthe anipisthundhi… maybe destiny was slowly writing our story.
 
 Nuv SVS lo chadivavu ani telisinappudu nenu shock ayyanu.
-Nuv lunch box kosam kindhaki vasthav ani telisi, nenu roju kindhaki vachhedi — just okkasari ninnu chudali ani. Kani malli chudaledu… maybe destiny wanted the right time.
+Nuv lunch box kosam kindhaki vasthav ani telisi, nenu roju kindhaki vache vadini okkasari ninnu chudali ani.Maybe destiny wanted the right time.
 
-Gnanu… ninnu chala ishtam.
+Gnanu… nuv antte chala istam.
 I truly love you from the bottom of my heart.
 
 Nitho unte baguntundhi… nuv navvithe inka baguntundhi…
@@ -25,7 +25,7 @@ Honestly, naaku life lo chala pedda expectations levu — nitho kalisi nadavadam
 
 I don’t want to force you or rush you.
 Nee decision ni respect chestha.
-Kani oka maata matram nijam — naa feelings marchavu.
+Kani oka maata matram nijam — naa feelings maravu.
 
 Will you give me a chance to be part of your life… forever❤️?
 Will you walk with me… not just now… but for life❤️?
